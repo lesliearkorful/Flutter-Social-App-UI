28d6e910b3b3c6bed266db56eaf8535df67bce7a
Leslie Arkorful <lesliearkorful@gmail.com>
2020-02-01 17:45:24 +0000

    Create Colors.dart

---
+++ lib/util/Colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart' show Color;
+
+class UIColors {
+  static Color navyBlue = Color(0xFF09074e);
+  static Color yellowOrange = Color(0xFFffae00);
+  static Color softRed = Color(0xFFff543f);
+  static Color softPurple = Color(0xFF7777f7);
+  static Color cardBackground = Color(0xFF172034);
+  static Color grey = Color(0xFFf7f7f7);
+}
