f9c0ceca31acb67da3f232bb881adffead9d9a84
Leslie Arkorful <lesliearkorful@gmail.com>
2020-02-01 18:09:09 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:social_app/ui/notifications/Notifications.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
